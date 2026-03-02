6e9069b0f17d74534572006f67f4e7f8171f89de
st1037-art <st1037@srmist.edu.in>
2026-03-02 14:15:32 +0530

    UC3

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,6 +1,14 @@
-public class OopsBanner{
+
+public class OopsBanner {
     public static void main(String[] args) {
-        // uc2:
-        System.out.println("OOPS");
+        System.out.println(String.join("", "   ***   ", "   ***   ", "******   ", "   *****"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", "**    ** ", " **     "));
+        System.out.println(String.join("", "**     **", "**     **", "**     **", "**      "));
+        System.out.println(String.join("", "**     **", "**     **", "**    ** ", " **     "));
+        System.out.println(String.join("", "**     **", "**     **", "******   ", "  ***   "));
+        System.out.println(String.join("", "**     **", "**     **", "**       ", "     ** "));
+        System.out.println(String.join("", "**     **", "**     **", "**       ", "      **"));
+        System.out.println(String.join("", " **   ** ", " **   ** ", "**       ", "     ** "));
+        System.out.println(String.join("", "   ***   ", "   ***   ", "**       ", "*****   "));
     }
 }
