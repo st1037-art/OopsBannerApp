40a830281adf9c1664178f734024c3c31e72dc00
st1037-art <st1037@srmist.edu.in>
2026-03-18 13:03:13 +0530

    UC5

---
+++ src/OopsBanner.java
@@ -0,0 +1,29 @@
+public class OopsBanner {
+    public static void main(String[] args) {
+
+        String[] bannerLines = createBanner();
+        printBanner(bannerLines);
+
+    }
+
+    public static String[] createBanner() {
+
+        return new String[] {
+
+            String.join("", "  *****          *****          *******         ******* "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *******          *******"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "  *****          *****         *                *******")
+        };
+    }
+
+    public static void printBanner(String[] bannerLines) {
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
